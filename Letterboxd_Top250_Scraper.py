8260afb894960722512f6d27f69c01a7148ac6be
andy <andyestevez27@gmail.com>
2024-10-30 14:49:05 -0400

    Added prompt for user input for selecting genre

--- Letterboxd_Top250_Scraper.py
+++ Letterboxd_Top250_Scraper.py
@@ -1,40 +1,42 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
+import inquirer
 
-
-
-url = "https://letterboxd.com/dave/list/official-top-250-narrative-feature-films/"
+# User Input for selecting genre or Top 100
+question = [
+    inquirer.List('genre',
+        message="What genre of film you are looking for?",
+        choices=['Horror', "Comedy", "Drama", "Romance", "Animation", "None - Let me see Top 100"],
+    ),
+]
+answers = inquirer.prompt(question)
+print(answers['genre'])
+if answers['genre'] == 'None - Let me see Top 100':
+    url = "https://letterboxd.com/dave/list/official-top-250-narrative-feature-films/"
+else:
+    url = "https://letterboxd.com/dave/list/official-top-250-narrative-feature-films/genre/" + answers['genre'].lower() + "/"
 # print(requests.get(url)) # to see Response code, want Response 200
+
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 browser = webdriver.Chrome(options=options) 
 browser.get(url)
 
+
+# get top films from letterboxd through <ul> 
 soup = BeautifulSoup(browser.page_source, 'html.parser')
 soup.prettify()
 data = soup.find('ul', class_="js-list-entries poster-list -p125 -grid film-list")
 movie_titles = data.find_all('span', class_="frame-title")
 titles = [title.text.strip() for title in movie_titles]
-#print(titles)
 data_rows = data.find_all('li')
-# print(data)
 
+
+# loop through all <li> for each film's info 
 for row in data_rows:
     movie_rank = row.find('p', class_="list-number").text
     movie_title = row.find('img')['alt']
-    movie_url = "/film/" + row.find('div')['data-film-slug'] + "/"
+    movie_url = "https://letterboxd.com/film/" + row.find('div')['data-film-slug'] + "/"
     print(movie_rank, movie_title, movie_url)
-
-# page = requests.get(url)
-
-# soup = BeautifulSoup(page.text, 'html')
-# soup.prettify()
-# # print(soup.findAll('ul')[20])
-# data = (soup.find('ul', class_="js-list-entries poster-list -p125 -grid film-list"))
-
-# movie_rank = (data.findAll('span', class_="frame-title"))
-
-# #text = [title.text.strip() for title in movie_titles]
-# print(movie_rank)
