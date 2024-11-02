dedb6d5e458c681618834f16409ab1a2c9310a27
andy <andyestevez27@gmail.com>
2024-11-02 13:33:23 -0400

    Added next page for scraping all items

--- baseballhats/baseballhats/spiders/hatspider.py
+++ baseballhats/baseballhats/spiders/hatspider.py
@@ -14,12 +14,14 @@ class HatSpider(scrapy.Spider):
             item['url'] = "https://www.mlbshop.com" + hat.css('div.product-image-container > a::attr(href)').get()
             item['image_url'] = "http:" + hat.css('div.product-image-container > a > img::attr(src)').get()
             
-            try: 
-                if hat.css('div.flag.flag-orange::text').get():
-                    item['stock'] = hat.css('div.flag.flag-orange::text').get()
-                else:
-                    item['stock'] = "In Stock"
-            except: 
+            if hat.css('div.flag.flag-orange::text').get():
+                item['stock'] = hat.css('div.flag.flag-orange::text').get()
+            else:
                 item['stock'] = "In Stock"
             
             yield item
+
+        next_page_available = response.css('li.next-page > a::attr(aria-disabled)').get()
+        if next_page_available == 'false':
+            next_page = "https://www.mlbshop.com" + response.css('li.next-page > a::attr(href)').get()
+            yield response.follow(next_page, callback=self.parse)
--- goodreads_fiction/goodreads_fiction/spiders/bookspider.py
+++ goodreads_fiction/goodreads_fiction/spiders/bookspider.py
@@ -18,14 +18,6 @@ class BookSpider(scrapy.Spider):
             item['score'] = booklist.css('a[href="#"][onclick^="Lightbox"]::text').get()[7:]
             item['url'] = base_url + booklist.css('a.bookTitle::attr(href)').get()
             yield item
-            # yield {
-            #     'rank': booklist.css('td.number::text').get(),
-            #     'title': booklist.css('span[itemprop="name"][role="heading"]::text').get(),
-            #     'author': booklist.css('a.authorName > span::text').get(),
-            #     'average-rating': booklist.css('span.minirating::text').get()[1:5] + "/5.00",
-            #     'score': booklist.css('a[href="#"][onclick^="Lightbox"]::text').get()[7:],
-            #     'url': base_url + booklist.css('a.bookTitle::attr(href)').get()
-            # }
 
         next_page = base_url + response.css('a.next_page').attrib['href']
         next_page_num = int(response.css('a.next_page').attrib['href'][-1])-1
